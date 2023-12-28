f4e0584a50c7dc1abec9c03aa9574e6cef516296
Ivan R. <ivbl21@yandex.ru>
2023-12-28 12:25:01 +0300

    DataTypes added

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var n: Int = 10
+    var l: Long = 100
+
+    var b: Boolean = false
+
+    var d: Double = 4.23
+    var f: Float = 4.23F
+
+    var s: String = "Amigoscode"
+    var c: Char = 'C'
+
+    var any: Any = 17
+    var any1: Any = "Isostatic"
+}
---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    //example how to use variables
+    var name: String = "Jamal"
+    var age = 29
+//    var age: String = 14 //is not correct type
+
+//    println(name)
+//    println(age)
+
+    print("Name is: $name and age is: $age")
+}
