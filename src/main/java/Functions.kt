e7e80d2d80296af95466eef61b6d8539c66312a9
ivan <ivbl21@yandex.ru>
2024-01-14 08:05:20 +0300

    several funcs created

--- src/main/java/Functions.kt
+++ src/main/java/Functions.kt
@@ -22,3 +22,31 @@ fun testDefaultArgs(x: Int = 1, y: Int = 8) {
     println(x + y)
 }
 
+
+/* ---------------------------------------------------
+Demonstration  of kotlin perfection.
+Instead overloading in Java, we can use function default args.
+Example from Kotlin Koans:
+
+public String foo(String name, int number, boolean toUpperCase) {
+    return (toUpperCase ? name.toUpperCase() : name) + number;
+}
+
+public String foo(String name, int number) {
+    return foo(name, number, false);
+}
+
+public String foo(String name, boolean toUpperCase) {
+    return foo(name, 42, toUpperCase);
+}
+
+public String foo(String name) {
+    return foo(name, 42);
+}
+*/
+
+//the same thing in Kotlin prevents multiple overloading:
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean) {
+    (if (toUpperCase) name.toUpperCase() else name) + number
+}
+//--------------------------------------------------------
